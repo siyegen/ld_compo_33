fa6e8b2f92df143e6106b935a6d55151317940c1
David Tomberlin <siyegen@gmail.com>
2015-08-22 00:34:33 -0700

    still messing around

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 console.log("Cool code!");
 
+function lerp(v0, v1, t) {
+  return (1-t)*v0 + t*v1;
+}
+
+function normalAcc(v0, v1) {
+  return lerp(v0, v1, .5);
+}
+
 class Game {
   constructor(){
     this.width = 1000;
     this.height = 800;
 
+    this.inputState = {
+      moving: false,
+      buttons: {
+        RIGHT: false,
+        LEFT: false,
+      },
+    };
+
     this.renderer = PIXI.autoDetectRenderer(this.width, this.height);
     this.stage = new PIXI.Container();
 
-    let sprite = new PIXI.Sprite.fromImage('./images/test-player.png');
+    this.grid = new PIXI.Graphics();
+
+    let texture = new PIXI.Texture.fromImage('./images/test-sky.png');
+    let tilingSprite = new PIXI.extras.TilingSprite(texture, this.width+300, this.height+300);
+
+    let sprite = new PIXI.Sprite.fromImage('./images/moorawr.png');
     this.player = new Player(new PIXI.Point(150, 80));
     sprite.anchor.x = 0.5, sprite.anchor.y = 0.5;
     sprite.position = this.player.position;
+    this.stage.addChild(this.grid);
     this.stage.addChild(sprite);
+    this.stage.addChildAt(tilingSprite, 0);
   }
 
   update() {
@@ -20,17 +43,45 @@ class Game {
       this.player.position.x += 1*this.player.direction.x;
     }
     if (this.player.direction.y != 0) {
-      
+      this.player.position.y += 1*this.player.direction.y;
     }
     this.player.direction.x = 0;
     this.player.direction.y = 0;
   }
 
   render() {
+    this.grid.clear();
+    this.grid.lineStyle(2, 0x886622, 0.8);
+    for (let i = 10; i >= 0; i--) {
+      this.grid.moveTo(i*50, 0);
+      this.grid.lineTo(i*50, this.height);
+    }
+    for (let i = 10; i >= 0; i--) {
+      this.grid.moveTo(0, i*50);
+      this.grid.lineTo(this.width, i*50);
+    }
     this.renderer.render(this.stage);
   }
 
+  handleInput() {
+    if (this.inputState.buttons.LEFT == true) {
+      this.player.direction.x = -1;
+    } else if (this.inputState.buttons.RIGHT == true) {
+      this.player.direction.x = 1;
+    } else {
+      this.player.direction.x = 0;
+    }
+    if (this.inputState.buttons.DOWN == true) {
+      this.player.direction.y = 1;
+    } else if (this.inputState.buttons.UP == true) {
+      this.player.direction.y = -1;
+    } else {
+      this.player.direction.y = 0;
+    }
+  }
+
   loop() {
+    this.handleInput();
     this.update();
     this.render();
     requestAnimationFrame(() => this.loop());
@@ -50,6 +101,10 @@ class Player {
   }
 }
 
+class Level {
+
+}
+
 let game = new Game();
 game.start();
 
@@ -62,25 +117,21 @@ let keyConfig = {
   39: "CAMRIGHT",
   32: "SPACE",
   90: "ZOOM",
-  70: "FOLLOW",
 };
+
+window.addEventListener('keyup', function(e) {
+  let key = keyConfig[e.keyCode];
+
+  if (key != undefined) {
+    game.inputState.buttons[key] = false;
+  }
+});
 
 window.addEventListener('keydown', function(e) {
   let key = keyConfig[e.keyCode];
-  switch(key) {
-    case "FOLLOW":
-      break;
-    case "UP":
-      game.player.direction.y = -1;
-      break;
-    case "DOWN":
-      game.player.direction.y = 1;
-      break;
-    case "LEFT":
-      game.player.direction.x = -1;
-      break;
-    case "RIGHT":
-      game.player.direction.x = 1;
-      break;
+
+  if (key != undefined) {
+    game.inputState.buttons[key] = true;
   }
+
 });
--- public/main.js
+++ public/main.js
@@ -1,11 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 console.log("Cool code!");
+
+function lerp(v0, v1, t) {
+  return (1 - t) * v0 + t * v1;
+}
+
+function normalAcc(v0, v1) {
+  return lerp(v0, v1, .5);
+}
 
 var Game = (function () {
   function Game() {
@@ -14,36 +22,82 @@ var Game = (function () {
     this.width = 1000;
     this.height = 800;
 
+    this.inputState = {
+      moving: false,
+      buttons: {
+        RIGHT: false,
+        LEFT: false
+      }
+    };
+
     this.renderer = PIXI.autoDetectRenderer(this.width, this.height);
     this.stage = new PIXI.Container();
 
-    var sprite = new PIXI.Sprite.fromImage('./images/test-player.png');
+    this.grid = new PIXI.Graphics();
+
+    var texture = new PIXI.Texture.fromImage('./images/test-sky.png');
+    var tilingSprite = new PIXI.extras.TilingSprite(texture, this.width + 300, this.height + 300);
+
+    var sprite = new PIXI.Sprite.fromImage('./images/moorawr.png');
     this.player = new Player(new PIXI.Point(150, 80));
     sprite.anchor.x = 0.5, sprite.anchor.y = 0.5;
     sprite.position = this.player.position;
+    this.stage.addChild(this.grid);
     this.stage.addChild(sprite);
+    this.stage.addChildAt(tilingSprite, 0);
   }
 
   _createClass(Game, [{
-    key: "update",
+    key: 'update',
     value: function update() {
       if (this.player.direction.x != 0) {
         this.player.position.x += 1 * this.player.direction.x;
       }
-      if (this.player.direction.y != 0) {}
+      if (this.player.direction.y != 0) {
+        this.player.position.y += 1 * this.player.direction.y;
+      }
       this.player.direction.x = 0;
       this.player.direction.y = 0;
     }
   }, {
-    key: "render",
+    key: 'render',
     value: function render() {
+      this.grid.clear();
+      this.grid.lineStyle(2, 0x886622, 0.8);
+      for (var i = 10; i >= 0; i--) {
+        this.grid.moveTo(i * 50, 0);
+        this.grid.lineTo(i * 50, this.height);
+      }
+      for (var i = 10; i >= 0; i--) {
+        this.grid.moveTo(0, i * 50);
+        this.grid.lineTo(this.width, i * 50);
+      }
       this.renderer.render(this.stage);
     }
   }, {
-    key: "loop",
+    key: 'handleInput',
+    value: function handleInput() {
+      if (this.inputState.buttons.LEFT == true) {
+        this.player.direction.x = -1;
+      } else if (this.inputState.buttons.RIGHT == true) {
+        this.player.direction.x = 1;
+      } else {
+        this.player.direction.x = 0;
+      }
+      if (this.inputState.buttons.DOWN == true) {
+        this.player.direction.y = 1;
+      } else if (this.inputState.buttons.UP == true) {
+        this.player.direction.y = -1;
+      } else {
+        this.player.direction.y = 0;
+      }
+    }
+  }, {
+    key: 'loop',
     value: function loop() {
       var _this = this;
 
+      this.handleInput();
       this.update();
       this.render();
       requestAnimationFrame(function () {
@@ -51,7 +105,7 @@ var Game = (function () {
       });
     }
   }, {
-    key: "start",
+    key: 'start',
     value: function start() {
       console.log("Start game");
       document.body.appendChild(this.renderer.view);
@@ -69,6 +123,10 @@ var Player = function Player(position) {
   this.direction = { x: 0, y: 0 };
 };
 
+var Level = function Level() {
+  _classCallCheck(this, Level);
+};
+
 var game = new Game();
 game.start();
 
@@ -80,27 +138,22 @@ var keyConfig = {
   37: "CAMLEFT",
   39: "CAMRIGHT",
   32: "SPACE",
-  90: "ZOOM",
-  70: "FOLLOW"
+  90: "ZOOM"
 };
+
+window.addEventListener('keyup', function (e) {
+  var key = keyConfig[e.keyCode];
+
+  if (key != undefined) {
+    game.inputState.buttons[key] = false;
+  }
+});
 
 window.addEventListener('keydown', function (e) {
   var key = keyConfig[e.keyCode];
-  switch (key) {
-    case "FOLLOW":
-      break;
-    case "UP":
-      game.player.direction.y = -1;
-      break;
-    case "DOWN":
-      game.player.direction.y = 1;
-      break;
-    case "LEFT":
-      game.player.direction.x = -1;
-      break;
-    case "RIGHT":
-      game.player.direction.x = 1;
-      break;
+
+  if (key != undefined) {
+    game.inputState.buttons[key] = true;
   }
 });
 
